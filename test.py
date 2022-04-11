e13e87c5cc405fcdaf2a7f69a9001664215f90f9
Harsh B <harsh.bidar@gmail.com>
2022-04-11 10:53:11 +0530

    test commit

---
+++ test.py
@@ -0,0 +1,30 @@
+
+s = "Learning python is easy"
+#
+# s2 = [(index, item) for index, item in enumerate(s)]
+#
+# print(s2)
+
+# l = [1, 2, 3]
+#
+# l1 = [4, 5, 6]
+#
+# l.extend(l1)
+#
+# print(l)
+#
+#
+# list = [item[::-1] if len(item) % 2 == 0 else item for item in ['aaa', 'bbbb', 'ccccc']]
+
+print("hi") if 4 > 5 else print("hello")
+
+
+
+# w = s.split()
+#
+# words = [item[::-1] for item in w]
+#
+# s1 = " ".join(words)
+#
+# print(str(s1))
+
